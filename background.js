5c4312ad75d3688e6bedb3188ee3deb2423a48f1
Jonas <git@jonasscheiwiller.ch>
2025-02-14 14:27:17 +0100

    feat: listen to click on extension button

---
+++ background.js
@@ -0,0 +1,19 @@
+console.log('🏄‍♂️ — Custon WSL Extension is running!');
+
+// browser.runtime.onInstalled.addListener(() => {
+//     console.log('WSL Extension installed or updated.');
+// });
+
+browser.browserAction.onClicked.addListener(() => {
+    console.log("WSL Toolbar button clicked!");
+});
+
+
+
+browser.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+        alert("Hello from your extension!");
+    }
+});
+
